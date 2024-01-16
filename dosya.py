f1ded7dea4c37576b29072bde2ecf8f29cea187b
İbrahim EDİZ <edizdesign@gmail.com>
2024-01-16 07:39:37 +0000

    16012023

--- dosya.py
+++ dosya.py
@@ -1,12 +1,12 @@
 import os
 import shutil
 liste = ["Mehmet","Cagatay","Omer","Ugur","Mustafa"]
-fileName = "pandas01.ipynb"
+fileName = "ornekPandas.ipynb"
 for item in liste:
     adres = os.path.join("Egzersizler",item)
     if not os.path.exists(adres):
         os.mkdir(adres)
-    open(os.path.join("Egzersizler",item,"ilk.ipynb"),"wb")
-    # shutil.copy(f"Egzersizler/SoruCevap/{fileName}",f"Egzersizler/{item}/{fileName}")
+    # open(os.path.join("Egzersizler",item,fileName),"wb")
+    shutil.copy(f"Egzersizler/SoruCevap/{fileName}",f"Egzersizler/{item}/{fileName}")
     
 
